8c3418f8ae8be472032a6c9ff01e78fd2fbc2137
Pedro Garcia <pedrognunes139@gmail.com>
2022-09-13 21:22:12 -0300

    adding save, load and check function

--- SudokuTable.py
+++ SudokuTable.py
@@ -1,18 +1,19 @@
 from tabulate import tabulate
-
+import copy
 class SudokuTable:
     sudoku = []
     sudokuSolved = []
+    mask = []
     
     def __init__(self, table):
-        solvedTable = 0
+        currentTable = 0
         
         for line in table:
             temp = []
             
             #checa se mudou de tabela
             if line == "\n":
-                solvedTable = 1
+                currentTable += 1
                 continue
             
             
@@ -23,16 +24,20 @@ class SudokuTable:
                     else:
                         temp.append(c)
                         
-            if solvedTable == 0:
+            if currentTable == 0:
                 self.sudoku.append(temp)
-            else:
+            elif currentTable == 1:
                 self.sudokuSolved.append(temp)
-    
+            else:
+                self.mask.append(temp)
+                
+        self.mask = copy.deepcopy(self.sudoku)
+        
     def printTable(self):
         print(tabulate(self.sudoku, tablefmt='fancy_grid'))
         
     def isFixedPosition(self, line, row):
-        if self.sudoku[line][row] == self.sudokuSolved[line][row]:
+        if self.mask[line-1][row-1] != "":
             return True
         else:
             return False
@@ -41,10 +46,52 @@ class SudokuTable:
         if self.isFixedPosition(line, row):
             print("Fixed Position!")
         else:
-            self.sudoku[line][row] = value
+            self.sudoku[line-1][row-1] = value
 
     def removeNumber(self, line, row):
         if self.isFixedPosition(line, row):
             print("Fixed Position!")
         else:
-            self.sudoku[line][row] = ""
+            self.sudoku[line-1][row-1] = ""
+            
+    def isGameCorrect(self):
+        for i, line in enumerate(self.sudoku) :
+            for j, c in enumerate(line) :
+                if c == self.sudokuSolved[i][j] :
+                    continue
+                else:
+                    print("Game is incorrect")
+                    return False
+        print("Game is Correct ! Congrats!")
+        return True
+    
+    def saveGame(self,name):
+        with open(name + ".txt", "w") as f:
+            
+            for line in self.sudoku :
+                for c in line :
+                    if c == "":
+                        f.write(".")
+                    else:
+                        f.write(str(c))
+                f.write("\n")
+            f.write("\n")
+            
+            for line in self.sudokuSolved :
+                for c in line :
+                    if c == "":
+                        f.write(".")
+                    else:
+                        f.write(str(c))                    
+                f.write("\n")
+            f.write("\n")
+            
+            for line in self.mask :
+                for c in line :
+                    if c == "":
+                        f.write(".")
+                    else:
+                        f.write(str(c))
+                f.write("\n")
+
+
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from SudokuTable import SudokuTable
 
-stable = open("table1.txt")
+stable = open("savedGame.txt")
 tablel = stable.readlines()
 
 stable.close
 
 tb = SudokuTable(tablel)
 
+def menu():
+    print("---- BEM VINDO AO SUDOKU! ----")
+    print("----   1 - NOVO JOGO      ----")
+    print("----   2 - CARREGAR JOGO  ----")
+    print("----   2 - SAIR           ----")
+    print("------------------------------")
+    op = input()
+    return op
 
-tb.printTable()
-
-
-
+def tableMenu():
+    print("----   BEM VINDO AO SUDOKU!  ----")
+    print("----   1 - Adicionar Jogada  ----")
+    print("----   2 - Remover Jogada    ----")
+    print("----   3 - Salvar Jogo       ----")
+    print("----   4 - Conferir Jogo     ----")
+    print("----   5 - Voltar            ----")
+    print("----   6 - Sair              ----")
+    print("---------------------------------")
+    op = input()
+    return op
